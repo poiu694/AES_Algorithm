3597aead542bc5b20001421c959d10f85c7b4258
Lee <43488305+poiu694@users.noreply.github.com>
2022-04-12 17:50:00 +0900

    [Fix] first of all, state fills to column

--- aes_algorithm.cpp
+++ aes_algorithm.cpp
@@ -11,14 +11,30 @@ int	get_round(string key)
 	return (0);
 }
 
+string	get_state(string plaintext)
+{
+	string	state;
+
+	state = "";
+	for (int i=0; i<8; i+=2)
+	{
+		for (int j=i; j<32; j+=8)
+		{
+			state += plaintext.substr(j, 2);
+		}
+	}
+	state = get_binary_bits(state);
+	return (state);
+}
+
 string	aes_algorithm(string plaintext, string key)
 {
 	string	encrypt;
 	string	bin_key;
 	int		round;
 
-	encrypt = get_binary_bits(plaintext);
-	bin_key = get_binary_bits(key);
+	encrypt = get_state(plaintext);
+	bin_key = get_state(key);
 	round = get_round(bin_key);
 	add_round_key(encrypt, bin_key, 0);
 	for (int i=1; i<=round; i++)
